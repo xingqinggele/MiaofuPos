84afc2569e4ed3cc86c82b9e2c2e194ca1950df2
1 <a>
2022-11-11 14:12:51 +0800

    第一次提交

---
+++ app/src/main/java/com/example/miaofupos/net/Urls.java
@@ -0,0 +1,8 @@
+package com.example.miaofupos.net;
+
+public class  Urls {
+//    public static String commUrls = "http://192.168.3.150:8083/"; // 本地
+//    public static String socketUrls = "ws://192.168.3.150:8083/socket/";  //长连接地址
+    public static String commUrls = "http://49.233.32.100:9003/";  //远程
+    public static String socketUrls = "ws://49.233.32.100:9003/socket/";  //长连接地址
+}
